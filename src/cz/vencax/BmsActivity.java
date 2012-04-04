984f6ffc9cc503e023599180239f836334d98b7c
Vaclav Klecanda <vencax77@gmail.com>
2012-04-04 17:01:56 +0200

    service is running

--- src/cz/vencax/BmsActivity.java
+++ src/cz/vencax/BmsActivity.java
@@ -3,23 +3,28 @@ package cz.vencax;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
-import java.util.TimerTask;
-
-import cz.vencax.service.AbstractBMSService;
-import cz.vencax.service.CellInfo;
-import cz.vencax.service.DebugBMSService;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Messenger;
 import android.widget.GridView;
+import android.widget.TextView;
+import cz.vencax.service.BMSService;
+import cz.vencax.service.CellInfo;
 
 public class BmsActivity extends Activity {
 	
-	private AbstractBMSService _service = null;
 	private Timer _autoUpdate;
 	private List<CellInfo> mData = null;
+	
+	private static final int ONGOING_NOTIFICATION = 143242;
+	
+	Messenger mService = null;
+//	final Messenger mMessenger = new Messenger(new IncomingHandler());
+	TextView textStatus, textIntValue, maxLoaderVoltage;
+    boolean mIsBound;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,11 +34,92 @@ public class BmsActivity extends Activity {
 		for(int i=0; i<16; i++) {
 			mData.add(new CellInfo());
 		}
-		this._service = new DebugBMSService(mData);
+		
+		textStatus = (TextView)findViewById(R.id.textView1);
+		textIntValue = (TextView)findViewById(R.id.textView2);
+		maxLoaderVoltage = (TextView)findViewById(R.id.maxLoaderVoltage);
+		
+		startBMSService();
 		
 		GridView gridview = (GridView) findViewById(R.id.gridView1);
 		ImageAdapter ia = new ImageAdapter(this, mData);
 	    gridview.setAdapter(ia);
+	}
+	
+//	private void CheckIfServiceIsRunning() {
+//        //If the service is running when the activity starts, we want to automatically bind to it.
+//        if(BMSService.isRunning()) {
+//            doBindService();
+//        } else {
+//        	startService(new Intent(BmsActivity.this, BMSService.class));
+//        }
+//    }
+	
+	
+//	class IncomingHandler extends Handler {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            switch (msg.what) {
+//            case BMSService.MSG_SET_INT_VALUE:
+//                textIntValue.setText("Int Message: " + msg.arg1);
+//                break;
+//            case BMSService.MSG_SET_STRING_VALUE:
+//                String str1 = msg.getData().getString("str1");
+//                textIntValue.setText("Str Message: " + str1);
+//                break;
+//            default:
+//                super.handleMessage(msg);
+//            }
+//        }
+//    }
+//    private ServiceConnection mConnection = new ServiceConnection() {
+//        public void onServiceConnected(ComponentName className, IBinder service) {
+//            mService = new Messenger(service);
+//            textStatus.setText("Attached.");
+//            try {
+//                Message msg = Message.obtain(null, BMSService.MSG_REGISTER_CLIENT);
+//                msg.replyTo = mMessenger;
+//                mService.send(msg);
+//            } catch (RemoteException e) {
+//                // In this case the service has crashed before we could even do anything with it
+//            }
+//        }
+//
+//        public void onServiceDisconnected(ComponentName className) {
+//            // This is called when the connection with the service has been unexpectedly disconnected - process crashed.
+//            mService = null;
+//            textStatus.setText("Disconnected.");
+//        }
+//    };
+
+	
+//	void doBindService() {
+//        bindService(new Intent(this, BMSService.class), mConnection, Context.BIND_AUTO_CREATE);
+//        mIsBound = true;
+//        textStatus.setText("Binding.");
+//    }
+//	
+//    void doUnbindService() {
+//        if (mIsBound) {
+//            // If we have received the service, and hence registered with it, then now is the time to unregister.
+//            if (mService != null) {
+//                try {
+//                    Message msg = Message.obtain(null, BMSService.MSG_UNREGISTER_CLIENT);
+//                    msg.replyTo = mMessenger;
+//                    mService.send(msg);
+//                } catch (RemoteException e) {
+//                    // There is nothing special we need to do if the service has crashed.
+//                }
+//            }
+//            // Detach our existing connection.
+//            unbindService(mConnection);
+//            mIsBound = false;
+//            textStatus.setText("Unbinding.");
+//        }
+//    }
+	
+	private void startBMSService() {
+		startService(new Intent(BmsActivity.this, BMSService.class));
 	}
 
 //	@Override
--- src/cz/vencax/service/DebugBMSService.java
+++ src/cz/vencax/service/DebugBMSService.java
@@ -2,21 +2,19 @@ package cz.vencax.service;
 
 import java.util.List;
 
-public class DebugBMSService extends AbstractBMSService {
-
+public class DebugBMSService {
+	
 	public DebugBMSService(List<CellInfo> data) {
-		super(data);
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	protected void obtainData() {
-		synchronized(this.data) {
-			for(CellInfo i : this.data) {
-				i.voltage = 3;
-				i.temp = 34;
-			}
-		}
+//		synchronized(this.data) {
+//			for(CellInfo i : this.data) {
+//				i.voltage = 3;
+//				i.temp = 34;
+//			}
+//		}
 	}
 
 }
